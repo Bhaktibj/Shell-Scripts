7a450f15a5a6478645e750dc03e147652937ad27
bhaktibj <bhaktibj402@gmail.com>
2020-02-13 12:57:01 +0530

    rename file

---
+++ basic_shell/airthmatic_operation.sh
@@ -0,0 +1,19 @@
+# basic airthmatic operations
+#!/bin/bash -x
+x=200;
+y=100;
+z=$(( $x + $y ))
+echo "$x + $y =" $z
+echo "*****************"
+echo "read input from user"
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+sum=$(( $a + $b ))
+sub=$(( $a - $b ))
+mul=$(( $a * $b ))
+div=$(( $a / $b ))
+echo "Sum is: " $sum
+echo "Sub is: " $sub
+echo "Mul is: " $mul
+echo "Div is: " $div
+
