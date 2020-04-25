541e9c66374ca732b790f52fc1c413e1eeb3d383
bhaktibj <bhaktibj402@gmail.com>
2020-04-25 13:10:22 +0530

    check horizontal, vertical and diagonal side

--- Logical_Shell_Scripts/TicTacToe.sh
+++ Logical_Shell_Scripts/TicTacToe.sh
@@ -91,5 +91,175 @@ function randomCheck(){    # this function is used to check random input
 }
 
 
+function checkTieCase(){  # this method is used check the game is tie
+
+	while [ ${board[$nonEmptyCount]} != 0 ] # check while board of non empty count is not equal to 0
+	do
+		if [ $nonEmptyCount -eq 9 ]   # check non empty count is equal to 9
+		then
+			printBoard   # then printBoard
+			echo "Game Is Tie"  # game is tie
+			winner=true   # assign winner equal to True
+			break     # braek
+		else
+			nonEmptyCount=$(($nonEmptyCount+1))  # else assign non Empty count equal to non empty count + 1
+		fi
+	done
+
+}
+
+function checkWinningMoveComputer(){  # This method is used to check Winning Move
+	counter=1  # counter is equal to 1
+	winMove=false  # Win move equal to false
+
+	for (( i=1; i<=3; i++ ))  # for i = 1 and i< = 3
+	do   # check borad of counter is equal to board of counter + $1 + $1
+		if [[ ${board[$counter]} == ${board[$counter+$1+$1]} ]] && [[ ${board[$counter+$1]} == 0 ]] &&[[ ${board[$counter]} == $computer ]]
+		then
+			computerP=$(( $counter+$1 )) # check computer player equal to counter + 1
+			echo " winning move is " $computerP # winning move is computer
+			board[$computerP]=$computer # board of computer player equal to computer
+			winMove=true # win move is equal to True
+			break  # break
+			# else if check borad of counter == board of counter + 1  and board of counter + $1 + $1 is equal to 0
+		elif [[ ${board[$counter]} == ${board[$counter+$1]} ]] && [[ ${board[$counter+$1+$1]} == 0 ]] && [[ ${board[$counter]} == $computer ]]
+		then
+			computerP=$(( $counter+$1+$1 )) # assign computer player equal to counter increament by counter + $1 + $1
+			echo "winning move is " $computerP  # winner is computer
+			board[$computerP]=$computer  # assign board of  computer equal to computer
+			winMove=true  # win move equal to True
+			break
+		elif [[ ${board[$counter+$1]} == ${board[$counter+$1+$1]} ]] && [[ ${board[$counter]} == '-' ]] && [[ ${board[$counter+$1]} == $computer ]]
+		then
+			computerP=$counter
+			echo "winning move is " $computerP
+			board[$computerP]=$computer
+			winMove=true
+			break
+		fi
+	counter=$(( $counter+$2 ))
+	done
+}
+
+function checkWinningMovePlayer(){  # This method is used to check Winning move player
+        counterPlayer=1  # counter player is equal to 1
+        winMovePlayer=false
+        for (( i=1; i<=3; i++ )) # for loop i = 1 and i < =3
+        do
+                if [[ ${board[$counterPlayer]} == ${board[$counterPlayer+$1+$1]} ]] && [[ ${board[$counterPlayer+$1]} == 0 ]] &&[[ ${board[$counterPlayer]} == $player ]]
+                then
+                        playerP=$(( $counterPlayer+$1 ))
+                        echo "player winning move is " $playerP
+                        board[$playerP]=$player
+                        winMovePlayer=true
+                        break
+                elif [[ ${board[$counterPlayer]} == ${board[$counterPlayer+$1]} ]] && [[ ${board[$counterPlayer+$1+$1]} == 0 ]] && [[ ${board[$counterPlayer]} == $player ]]
+                then
+                        playerP=$(( $counterPlayer+$1+$1 ))
+                        echo "player winning move is " $playerP
+                        board[$playerP]=$player
+                        winMovePlayer=true
+                        break
+                elif [[ ${board[$counterPlayer+$1]} == ${board[$counterPlayer+$1+$1]} ]] && [[ ${board[$counterPlayer]} == '-' ]] && [[ ${board[$counterPlayer+$1]} == $player ]]
+                then
+                        playerP=$counter
+                        echo "player winning move is " $playerP
+                        board[$playerP]=$player
+                        winMovePlayer=true
+                        break
+                fi
+        counterPlayer=$(( $counterPlayer+$2 ))
+        done
+}
+
+function userInput() {   # This method is used to user input
+	row=1    # row equal to 1
+	column=3  # column equal to 3
+	winMovePlayer=false  # player win move = false
+	read -p "Enter you position in between 1 to 9: " POSITION  # enter the position
+	checkWinningMovePlayer $row $column    # check winning move  player in row and column
+        checkWinningMovePlayer $column $row
+	if [ $winMovePlayer == false ] # if win move player equal to false
+	then   # then
+		if [ ${board[$POSITION]} -eq $TAIL ]   # check if board of postion equal to TAIL
+		then    # then
+			echo "player turn"
+			board[$POSITION]=$player   # board of position = player
+			printBoard   # print board
+			turn=$(( $turn + 1 ))  # turn = turn increament by 1
+		else
+			echo "Invalid input"   # invalid input
+			userInput # user input
+		fi
+	fi
+		playerTurn=false  # player turn equal to false
+
+}
 
 
+function computerInput(){   # This method is used to check computer input
+	row=1  # row equal to 1
+	column=3  # column equal to 3
+	winMove=false  # win move equal to False
+
+	echo "Computer is Playing"   # computer is playing
+	checkWinningMoveComputer $row $column   # call winning move computer
+	checkWinningMoveComputer $column $row  # call winning move computer
+	cornerCheck # call corner check
+	middleCheck # call middle check function
+	randomCheck  # call random check function
+	POSITION=$((RANDOM%9+1))  # position equal to random 9+1
+
+	if [ $winMove == false ]  # check win move equal to false
+	then   # then
+		if [ ${board[$POSITION]} == $TAIL ]  # check board of position equal to tail
+		then   # then
+			board[$POSITION]=$computer   # board of position assign computer
+		else
+			echo "Wrong move "
+			computerInput  # wrong computer input
+		fi
+	fi
+		playerTurn=true # player turn equal to true
+}
+
+
+function ticTacToe(){    # This method is used toe tictact
+
+	checkingToss=$((RANDOM%2))  # assign checktoss eqaual to Random%2
+	if [[ $checkingToss -eq $HEAD ]]  # check if chekingtoss is equal to HEAD means 1
+	then
+		playerTurn=true  # then player turn assign true
+		player='X'  # player X
+		computer='O' # and computer 'o'
+		echo "Player play first"
+		printBoard  # call printboard
+	else
+		player='X'  # else player = 'X'
+		computer='O'  # else player 'O'
+		echo "computer play first"
+	fi
+}
+
+boardInitialize # call board initialize funstion
+ticTacToe # call tictactoe
+
+while [ $winner == false ]  # while winner equal to false
+do
+
+		printBoard # print board
+		if [ $playerTurn == true ]  # check if playerTurn == True
+		then  # then
+			userInput  # call user input
+			checkHorizontalCase $player   # check horizontal case and pass player input
+			checkVerticalCase $player # check Vertical case and pass player input
+			checkDiagonalCase $player # pass player input and check
+			checkTieCase $player # check tie case
+		else  # else
+			computerInput  # computer input
+			checkHorizontalCase $computer  # pass computer input and check horizontal case
+      checkVerticalCase $computer # pass computer input and  check Vertical case
+      checkDiagonalCase $computer # pass computer input and check Diagonal case
+			checkTieCase $computer  # pass computer input and check Tie case
+		fi
+done
